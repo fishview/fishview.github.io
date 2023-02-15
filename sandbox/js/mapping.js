d521fe1c1bb9b6829f65e4fad68f88c39a45fb94
Tim Lam <63378627+fishview@users.noreply.github.com>
2023-02-15 14:54:59 +0800

    Update mapping.js

--- sandbox/js/mapping.js
+++ sandbox/js/mapping.js
@@ -1,4 +1,4 @@
-<!-- Map -->							
+<!-- Map -->
 	var map = L.map('map', {
 			attributionControl: false,
 			center: [10, -165],
@@ -10,8 +10,8 @@
 			period: "P1D",
 			loadingTimeout: 15000,
 			},
-	});	
-		
+	});
+
 <!-- Basemaps ---!>
 	var baseMaps = {
 			"Grayscale": greymap,
@@ -23,16 +23,16 @@
 			"Chlorophyll a after 2018": TimeLayerA,
 			"Oxygen at 120 m": TimeLayer4,
 			"Mixed layer thickness": TimeLayer5,
-	};		
+	};
 	baseMaps.Bathymetry.addTo(map);
-	
-<!-- Toolbar -->		
+
+<!-- Toolbar -->
 	var but1 = L.easyButton({
 		states: [{
 				stateName: 'zoom1',
 				icon:      '<span class="mybutton">&curarr;</span>',
 				title:     'Zoom to Hawaii',
-				onClick: function(btn, map) {       
+				onClick: function(btn, map) {
 					map.setView([24,-150],5);
 					btn.state('zoom2');
 				}
@@ -63,14 +63,14 @@
 								  onClick: function(){window.open('http://www.fishtoday.org/tagit/get-involved', '_blank');}
 								}
 							  ]
-							});									
+							});
 
 	var butL1 = L.easyButton({
 		states: [{
 				stateName: 'zoom1',
 				icon:      '<span class="mybutton">&rarrfs;</span>',
 				title:     'Turn OFF start/end points',
-				onClick: function(btn) {       
+				onClick: function(btn) {
 					migrationLayer1.hide();
 					map.removeControl(legend)
 					btn.state('zoom2');
@@ -91,7 +91,7 @@
 				stateName: 'zoom1',
 				icon:      '<span class="mybutton">&rarrfs;</span>',
 				title:     'Turn OFF IATTC tags',
-				onClick: function(btn) {       
+				onClick: function(btn) {
 					migrationLayer2.hide();
 					btn.state('zoom2');
 				}
@@ -110,7 +110,7 @@
 				stateName: 'zoom1',
 				icon:      '<span class="mybutton">&rarrfs;</span>',
 				title:     'Turn OFF Hawaii tags',
-				onClick: function(btn) {       
+				onClick: function(btn) {
 					migrationLayer3.hide();
 					btn.state('zoom2');
 				}
@@ -129,7 +129,7 @@
 				stateName: 'zoom1',
 				icon:      '<span class="mybutton">&rarrfs;</span>',
 				title:     'Turn OFF SPC tags',
-				onClick: function(btn) {       
+				onClick: function(btn) {
 					migrationLayer4.hide();
 					btn.state('zoom2');
 				}
@@ -143,9 +143,9 @@
 				}
 		}]
 	});
-	
+
 	var bbar = L.easyBar([but1, butL1]);
-	bbar.addTo(map);	      
+	bbar.addTo(map);
 
 <!-- Mouse position -->
   L.control.mousePosition({position: 'bottomleft'}).addTo(map);
@@ -156,7 +156,7 @@
 	L.control.logo2().addTo(map); // PIFG
 	//L.control.logo3().addTo(map); // MCSI
 	//L.control.logo4().addTo(map); // Offield
-	
+
 <!-- Animation -->
     var prad = .01; //30
 	var pbw = .01; // 3
@@ -188,7 +188,7 @@
 			labels = [];
 
 		// loop through our density intervals and generate a label with a colored square for each interval
-		div.innerHTML = '<h4>Months at liberty</h4>' 
+		div.innerHTML = '<h4>Months at liberty</h4>'
 		for (var i = 0; i < grades.length; i++) {
 			div.innerHTML +=
 				'<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
@@ -205,13 +205,13 @@
 			position: 'bottomleft'
 		});
 		legendA.onAdd = function(map) {
-			var src = alink + '?REQUEST=GetLegendGraphic&LAYER=' + alayer + '&PALETTE=' + app + '&COLORSCALERANGE=' + '16%2C32' + '&transparent=TRUE';
+			var src = alink + '?REQUEST=GetLegendGraphic&LAYER=' + alayer + '&PALETTE=' + app + '&COLORSCALERANGE=' + '0%2C32' + '&transparent=TRUE';
 			var div = L.DomUtil.create('div', 'info2 legend');
 			div.innerHTML +=
 				'<img src="' + src + '" alt="legend">';
 			return div;
-	};	
-	
+	};
+
 	var legendB = L.control({
 			position: 'bottomleft'
 		});
@@ -222,7 +222,7 @@
 				'<img src="' + src + '" alt="legend">';
 			return div;
 	};
-	
+
 	var legendC = L.control({
 			position: 'bottomleft'
 		});
@@ -233,7 +233,7 @@
 				'<img src="' + src + '" alt="legend">';
 			return div;
 	};
-	
+
 	var legendD = L.control({
 			position: 'bottomleft'
 		});
@@ -244,7 +244,7 @@
 				'<img src="' + src + '" alt="legend">';
 			return div;
 	};
-	
+
 	// Overlay layer
 	var olink = dlink + '?REQUEST=GetLegendGraphic&LAYER=' + dlayer + '&PALETTE=' + pp + '&COLORSCALERANGE=' + cc + '&transparent=TRUE';
 	var legendO = L.control({
@@ -257,8 +257,8 @@
 				'<img src="' + src + '" alt="legend">';
 			return div;
 	};
-		
-<!-- Layer control -->		
+
+<!-- Layer control -->
 	var overlayMaps = {
 			'Coastline': coastline,
 			'Grid lines': grid,
@@ -286,14 +286,14 @@
 			'PIFG16-17P0212-172781': kmlTimeLayer26,
 		};
 	L.control.layers(baseMaps,overlayMaps,{collapsed:true}).addTo(map);
-	
+
 <!-- Time control -->
 	var timeDimensionControl = new L.Control.TimeDimension({
 				position: 'bottomleft',
-				playReverseButton: true,				
+				playReverseButton: true,
 				minSpeed: 1,
 				maxSpeed: 20,
-				speedStep: 1,				
+				speedStep: 1,
 				autoPlay: true,
 				playerOptions: {
 				buffer: 5,
@@ -301,13 +301,13 @@
 				loop: true,
 				}
 	});
-	
+
 <!-- Display/ hide items -->
 var tCtrlOn = 0,
-	tBlay = 0, 
+	tBlay = 0,
 	tOverlay = 0;
-	
-	// Base change		
+
+	// Base change
 	map.on('baselayerchange', function(eventLayer) {
 		if (eventLayer.name == 'Chlorophyll a') {
 			map.removeControl(legendA);
@@ -326,20 +326,20 @@ var tCtrlOn = 0,
 			timeDimensionControl.addTo(this);
 			tCtrlOn = 1;
 			tBay = 1;
-			legendB.addTo(this);			
+			legendB.addTo(this);
 		} else if (eventLayer.name == 'Sea surface temperature'){
 			map.removeControl(legendB);
 			map.removeControl(legendC);
-			map.removeControl(legendD);			
+			map.removeControl(legendD);
 			if (tOverlay == 0) map.timeDimension.setAvailableTimes(Time0, 'replace');
 			timeDimensionControl.addTo(this);
 			tCtrlOn = 1;
 			tBlay = 1;
-			legendA.addTo(this);			
+			legendA.addTo(this);
 		} else if (eventLayer.name == 'Oxygen at 120 m'){
 			map.removeControl(legendA);
 			map.removeControl(legendB);
-			map.removeControl(legendD);			
+			map.removeControl(legendD);
 			if (tOverlay == 0) map.timeDimension.setAvailableTimes(Time0, 'replace');
 			timeDimensionControl.addTo(this);
 			tCtrlOn = 1;
@@ -348,12 +348,12 @@ var tCtrlOn = 0,
 		} else if (eventLayer.name == 'Mixed layer thickness'){
 			map.removeControl(legendA);
 			map.removeControl(legendB);
-			map.removeControl(legendC);			
+			map.removeControl(legendC);
 			if (tOverlay == 0) map.timeDimension.setAvailableTimes(Time0, 'replace');
 			timeDimensionControl.addTo(this);
 			tCtrlOn = 1;
 			tBlay = 1;
-			legendD.addTo(this);				
+			legendD.addTo(this);
 		} else {
 			map.removeControl(legendA);
 			map.removeControl(legendB);
@@ -362,8 +362,8 @@ var tCtrlOn = 0,
 			if (tOverlay == 0) {map.removeControl(timeDimensionControl);}
 			tCtrlOn = 0;
 			tBlay = 0;
-		} 
-	});	
+		}
+	});
 	// Overlay ON
 	map.on('overlayadd', function(eventLayer) {
 		if (eventLayer.name == 'Sea water velocity' || eventLayer.name == '24 degC SST') {
@@ -520,7 +520,7 @@ var tCtrlOn = 0,
                 map.timeDimension.setAvailableTimes(Time26, 'union');
             } else {
                 map.timeDimension.setAvailableTimes(Time26, 'replace');
-            }			
+            }
 		} else if (eventLayer.name == 'PIFG17-16P1575-167342') {
             tOverlay = tOverlay + 1;
             if (tCtrlOn == 0){
@@ -530,7 +530,7 @@ var tCtrlOn = 0,
             } else {
                 map.timeDimension.setAvailableTimes(Time27, 'replace');
             }}
-	});	
+	});
 	// Overlay OFF
 	map.on('overlayremove', function(eventLayer) {
 		if (eventLayer.name == 'Sea water velocity' || eventLayer.name == '24 degC SST') {
@@ -539,6 +539,6 @@ var tCtrlOn = 0,
 			if (tBlay == 0 && tOverlay < 1) {map.removeControl(timeDimensionControl);}
 		} else if (eventLayer.name == 'PIFG-LPRC 20139' || eventLayer.name == 'PIFG01-20139-115409' || eventLayer.name == 'PIFG02-16P1909-169364' || eventLayer.name == 'PIFG03-20573-117277' || eventLayer.name == 'PIFG04-16P1849-169355' || eventLayer.name == 'PIFG05-20138-115408' || eventLayer.name == 'PIFG06-16P1579-167346' || eventLayer.name == 'PIFG07-16P1855-169356' || eventLayer.name == 'PIFG08-16P1580-167347' || eventLayer.name == 'PIFG09-16P1587-167351' || eventLayer.name == 'PIFG10-16P1896-169361' || eventLayer.name == 'PIFG11-16P1916-169365' || eventLayer.name == 'PIFG12-16P2435-172777' || eventLayer.name == 'PIFG13-17P0206-172780' || eventLayer.name == 'PIFG14-17P0213-172782' || eventLayer.name == 'PIFG15-16P1903-169363' || eventLayer.name == 'PIFG16-17P0212-172781' || eventLayer.name == 'PIFG17-16P1575-167342' ) {
 			tOverlay = tOverlay - 1
-			if (tBlay == 0 && tOverlay < 1) {map.removeControl(timeDimensionControl);}			
-		}	
-	});	
+			if (tBlay == 0 && tOverlay < 1) {map.removeControl(timeDimensionControl);}
+		}
+	});
